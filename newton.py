27df7b65409d7cdbdcf997b5485e9c925dda506b
Anna Liang <annaliangwork@gmail.com>
2026-08-19 11:06:35 -0700

    Edit function

--- newton.py
+++ newton.py
@@ -1,23 +1,28 @@
-def newton(x_0, epsilon = 0.00001):
+def newton(x_0, epsilon=0.00001):
     x_t = x_0
-    h = 0.00001 # define a "small value"
+    h = 0.00001  # small value
     x_prev = 10
-    
+
     while abs(x_t - x_prev) > epsilon:
         x_prev = x_t
-        
-        first_deriv = (f(x_prev + h) - f(x_prev - h)) / (2 * h)
 
-        # 
-        second_deriv = (f(x_prev + h) - 2 * f(x_prev) + f(x_prev - h)) / (h ** 2)
+        # f'(x) = (f(x+h) - f(x)) / h
+        first_deriv = (f(x_prev + h) - f(x_prev)) / h
 
-        # Newton step x_t = x_t-1 - f'(x_t-1)/f''(x_t-1)
+        # f''(x) = (f(x+h) - 2f(x) + f(x-h)) / h^2
+        second_deriv = (
+            f(x_prev + h)
+            - 2 * f(x_prev)
+            + f(x_prev - h)
+        ) / (h ** 2)
+
+        # Newton's optimization step:
+        # x_new = x_old - f'(x_old) / f''(x_old)
         x_t = x_prev - (first_deriv / second_deriv)
 
     return x_t
 
 
 if __name__ == "__main__":
-    print
-        
-    
+    print(newton(x**2))
+
