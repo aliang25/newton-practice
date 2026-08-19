84577c30216b66f9378317a1e212b6163a911c8f
Anna Liang <annaliang@berkeley.edu>
2026-08-19 14:35:14 -0700

    splitting function into 4 different ones, adding docstring and format w/ ruff

--- newton.py
+++ newton.py
@@ -1,28 +1,31 @@
-def newton(x_0, epsilon=0.00001):
+def f(x):
+    """Return the value of f(x) = x^2."""
+    return x**2
+
+
+def first_deriv(x):
+    """Approximate the first derivative of f at x."""
+    h = 0.00001
+    return (f(x + h) - f(x)) / h
+
+
+def second_deriv(x):
+    """Approximate the second derivative of f at x."""
+    h = 0.00001
+    return (f(x + h) - 2 * f(x) + f(x - h)) / (h**2)
+
+
+def optimize(x_0, epsilon=0.00001):
+    """Find a minimum of f using Newton's optimization method."""
     x_t = x_0
-    h = 0.00001  # small value
     x_prev = 10
 
     while abs(x_t - x_prev) > epsilon:
         x_prev = x_t
-
-        # f'(x) = (f(x+h) - f(x)) / h
-        first_deriv = (f(x_prev + h) - f(x_prev)) / h
-
-        # f''(x) = (f(x+h) - 2f(x) + f(x-h)) / h^2
-        second_deriv = (
-            f(x_prev + h)
-            - 2 * f(x_prev)
-            + f(x_prev - h)
-        ) / (h ** 2)
-
-        # Newton's optimization step:
-        # x_new = x_old - f'(x_old) / f''(x_old)
-        x_t = x_prev - (first_deriv / second_deriv)
+        x_t = x_prev - (first_deriv(x_prev) / second_deriv(x_prev))
 
     return x_t
 
-# testing the function
-if __name__ == "__main__":
-    print(newton(x**2))
 
+if __name__ == "__main__":
+    print(optimize(10))
